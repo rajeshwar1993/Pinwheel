67db2790d9bb5db8c06cad8ba2f270d951de9003
Rajeshwar Rudra <rajeshwarrudra@Rajeshwars-MacBook-Pro.local>
2024-04-11 19:32:07 +0900

    column order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           Celebrating the Graphic Design Language of Public Spaces in India
         </div>
         <div className="grid grid-cols-5 gap-4">
-          <div className="col-span-5 lg:col-span-2 text-sm flex flex-col justify-center">
+          <div className="col-span-5 lg:col-span-2 text-sm flex flex-col justify-center order-last md:order-first">
             <div>
               <p>
                 With the advent of technology, the wealth of research studies
